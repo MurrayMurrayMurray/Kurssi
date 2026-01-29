c170e7a7e6b9244d48dcecd84d1f11b45f1ce910
Murray <s2401275@edu.bc.fi>
2026-01-29 12:50:23 +0200

    ruoka.
    ruoka

---
+++ RuokaAnnokset/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace RuokaAnnokset
+{
+    internal class Program
+    {
+        
+        enum P{
+            nautaa,
+            kanaa,
+            kasviksia,
+        }
+        enum L
+        {
+            perunaa,
+            riisiä,
+            pastaa,
+        }
+        enum K
+        {
+            pippuri,
+            hapanimelä,
+            chili,
+            tomaatti,
+        }
+        
+        static void Main(string[] args)
+        {
+
+            bool Oikein = false;
+
+
+            string valinta = " ";
+
+
+            Console.WriteLine("Tervetuloa ravintolaan");
+            // Luo uuden olion
+            // jos olion arvoja ei aseta
+            // ne ovat tyyppikohtaiset oletusarvot:
+            // int : 0, float 0, enum : ensimmäinen arvo
+            
+            
+                Console.WriteLine($"{pääruoat[i]}, ");
+                Console.WriteLine("I'm losing my mind I .. I sowed the seeds and watched them grow.");
+            } 
+            //if (valinta == pääruoat[i]) 
+                {
+
+                
+            }
+            Console.WriteLine();
+                    valinta = Console.ReadLine();
+            
+
+
+            // kirjoita vaihtoehdot : WriteLine
+            // Lue vastaus : ReadLine
+            // muuta Enum muotoon : Enum.TryParse
+            // tallenna tilaukseen
+            // jne..
+            
+        }
+    }
+}
