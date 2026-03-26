0024172f7c1e1cd9089752a329e9b65f1983ddb3
Murray <s2401275@edu.bc.fi>
2026-03-26 12:52:56 +0200

    nuoli muutokset

--- Nuolit/Program.cs
+++ Nuolit/Program.cs
@@ -3,15 +3,15 @@
     public enum K
     {
         puu = 3,
-        ruoste = 5,
-        kivi = 50,
+        ruoste = 50,
+        kivi = 5,
 
 
     }
     public enum S
     {
         lehti = 1,
-        kalanevä = 3,
+        kalanevä = 10,
         fasaaninsulka = 5
     }
     internal class Program
@@ -35,13 +35,13 @@
             string kärkisyöte = Console.ReadLine().ToLower();
             if (kärkisyöte == "kivi") ValittuKärki = K.kivi;
             else if (kärkisyöte == "ruoste") ValittuKärki = K.ruoste;
-
+            Console.WriteLine($"kärjeksi {ValittuKärki}, seuraavaksi?");
 
             Console.WriteLine("Valitse sulka (Lehti, Kalanevä, fasaaninsulka)");
             string sulkasyöte = Console.ReadLine().ToLower();
             if (sulkasyöte == "kalanevä") ValittuSulka = S.kalanevä;
             else if (sulkasyöte == "fasaaninsulka") ValittuSulka = S.fasaaninsulka;
-
+            Console.WriteLine($"Sulaksi {ValittuSulka}, ja nuolen pituudeksi?");
 
 
             bool Oikein = false;
